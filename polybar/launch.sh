b3d5d0f98268d7a415e693eb7eba67ee8ce5b62b
Eric da Silva <yemo.dasilva@gmail.com>
2020-04-16 09:07:07 +0200

    polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
+#/!bin/bash
+# Detect if secondary monitor is connected, if so, add same bar
+# see https://github.com/polybar/polybar/issues/763
 
-# Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+set -- $outputs
 
-echo "Polybar launched..."
+for m in $outputs; do
+	if [ $m == $1 ]
+	then
+		MONITOR1=$m polybar --reload top -c ~/.config/polybar/config &
+	fi
+done
+
+for m in $outputs; do
+	export MONITOR1=$1
+done
